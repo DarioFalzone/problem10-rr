f76385eb44262d353330c6640de7c536cb9650f8
DarioFalzone <darioefalzone95@gmail.com>
2021-05-11 12:27:56 -0300

    Login Page - Validation one - Validation two done

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,32 +1,16 @@
 // Codigo de práctica
 
-describe('Go to Google - Enter in search: wikipedia, click and search', () => {
-  it('Search, Google to Wikipedia', () => {
-      const googleUrl = 'http://google.com';
-      const wikipediaUrl = 'https://es.wikipedia.org/wiki/Wikipedia:Portada';
-      const inputGoogle = $('.gLFyf');
-      const linkWikipedia = $('//h3[@class="LC20lb DKV0Md"]');
+describe('Login page - Tests', () => {
+  it('Go to login page', () => {
+      const pageWeek = 'https://dariofalzone.github.io/problem10-rr/index.html';
 
-      //Enter google
-      browser.url(googleUrl);
-      expect(browser).toHaveTitle('Google');
-      inputGoogle.setValue('Wikipedia');
-      browser.keys(['Enter']);
-      linkWikipedia.click();
+      //Enter Login Page
+      browser.url(pageWeek);
+      //Validation one
+      expect(browser).toHaveTitle('Login');
+      //Validation Two
+      expect(browser).toHaveUrl('https://dariofalzone.github.io/problem10-rr/index.html')
+      //Validation Three
 
-      browser.pause(1000)
-
-      expect(browser).toHaveUrl('https://es.wikipedia.org/wiki/Wikipedia:Portada')
-      expect(browser).toHaveTitle('Wikipedia, la enciclopedia libre')
-
-      browser.pause(3000)
-      //Validation URL
-      if (browser.getUrl() === wikipediaUrl) {
-          console.log('----------------------SUCCESSFULY-----------------------------');
-      } else {
-          throw new Error('ERROR');
-      }
   });
-
-// Muchas gracias a la ayuda de santiago ottolini y sebastian sileoni por su ayuda en la resolucion de este ejercicio
 });
