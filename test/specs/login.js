c2b173224a9c8446defb55439c3971c92b9f5a8d
DarioFalzone <darioefalzone95@gmail.com>
2021-05-12 02:13:03 -0300

    PageObject - Login Page validation

--- test/specs/login.js
+++ test/specs/login.js
@@ -8,11 +8,38 @@ describe('Login page - ¡go to find error!', () => {
 	const pageLog = 'https://dariofalzone.github.io/problem10-rr/index.html';
 
 	describe('Test email input', () => {
-		it('Go to login page, write correct inf, click button login', () => {
-      browser.url(pageLog);
-      loginForm.testLogin('dario95@gmail.com', 'dario789');
-      expect(loginForm.errorText).toHaveText('Every validation has passed')
-      pauseTime();
+		it('Go to login page, write correct information', () => {
+			browser.url(pageLog);
+			loginForm.testLogin('dario95@gmail.com', 'dario789');
+			expect(loginForm.errorText).toHaveText('Every validation has passed');
+			pauseTime();
+		});
+
+		it('Go to login page, write incorrect information', () => {
+			browser.url(pageLog);
+			loginForm.testLogin('dario95.com', 'dario789');
+			expect(loginForm.errorText).toHaveText('The email entered is not valid');
+			pauseTime();
+		});
+	});
+
+	describe('Test password input', () => {
+		it('Go to login page, write incorrect pass (not greater  than 8)', () => {
+			browser.url(pageLog);
+			loginForm.testLogin('dario95@gmail.com', 'dario7899999');
+			expect(loginForm.errorText).toHaveText(
+				'Please enter a password greater than 8 characters'
+			);
+			pauseTime();
+		});
+
+		it('Go to login page, write incorrect pass (not number in pass)', () => {
+			browser.url(pageLog);
+			loginForm.testLogin('dario95@gmail.com', 'dario');
+			expect(loginForm.errorText).toHaveText(
+				'Please enter a password with one number minimum'
+			);
+			pauseTime();
 		});
 	});
 });
